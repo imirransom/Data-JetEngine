1198a4bb9d8d1aac61991d8fd38db92de61a0a02
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 14:27:50 -0500

    done with plotting random

--- mt.py
+++ mt.py
@@ -4,14 +4,23 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is acitve
 while True:
     # Make a random walk
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     plt.style.use("dark_background")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-               edgecolors="none", s=15)
+               edgecolors="none", s=1)
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+               s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
