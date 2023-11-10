555549ea13b186ee28383921654c4923359d7e36
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 13:43:36 -0500

    this is cool! Looks like a shooting star

--- mt.py
+++ mt.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 
 # Set chart title and label axes.
@@ -16,5 +16,7 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Set size of thick labels.
 ax.tick_params(axis="both", which="major", labelsize=14)
 
+# Set the range for each axis
+ax.axis([0, 1100, 0, 1100000])
 # ax.plot(squares)
 plt.show()
