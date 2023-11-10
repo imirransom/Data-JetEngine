bd7aeea972b17f17798101f87f6559f5edcfdac6
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 16:37:20 -0500

    working with json

--- python.repos.py
+++ python.repos.py
@@ -10,3 +10,20 @@ print(f"Status code: {r.status_code}")
 response_dict = r.json()
 # Process results.
 print(response_dict.keys())
+
+print(f"Total repositories: {response_dict['total_count']}")
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+# Examine the first repository
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print("\nSelected information about first repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+
