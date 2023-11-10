060763e415e487cca40dcc4894695af1322512d7
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 13:36:51 -0500

    plotting dots

--- mt.py
+++ mt.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
+
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,7 +14,7 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of thick labels.
-ax.tick_params(axis="both", labelsize=14)
+ax.tick_params(axis="both", which="major", labelsize=14)
 
-ax.plot(squares)
+# ax.plot(squares)
 plt.show()
