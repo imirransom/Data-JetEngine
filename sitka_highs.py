69162a4f15f25c2cc0c73d99d8626c8f996105fc
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 15:46:42 -0500

    done with csv files

--- sitka_highs.py
+++ sitka_highs.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -13,23 +13,32 @@ with open(filename) as f:
         print(index, column_header)
 
     # Get high temperatures from this file.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     highs = []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
     print(highs)
 
+# Plot high and low temperatures
 plt.style.use("dark_background")
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c ="red")
+ax.plot(dates, highs, c ="red", alpha=0.5)
+ax.plot(dates, lows, c ="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures - 2018", fontsize=24)
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+title = "Daily high and low temperatures - 2018\nDeath valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
