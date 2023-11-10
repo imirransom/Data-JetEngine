2e4dad9cf180111f6997272efb65149d3c115c50
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-10 14:44:04 -0500

    doing random dice

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from plot.graph_objs import Bar, Layout
+from plotly import offline
 
 from die import Die
 
@@ -7,8 +9,14 @@ die = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(22):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analyze results
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
